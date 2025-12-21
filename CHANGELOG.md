0f8126677daa8b18db69c3d12d4ae55613ca6b62
Kushal Das <mail@kushaldas.in>
2025-12-21 11:02:55 +0100

    feat: v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,13 @@
 
+<a id='changelog-0.2.0'></a>
+# 0.2.0 — 2025-12-21
+
+## Added
+
+- jwcrypto integration.
+- Documentation is live at https://hsmkey.rtfd.io
+- Added `examples` code.
+
 <a id='changelog-0.1.0'></a>
 # 0.1.0 — 2025-12-20
 
--- docs/conf.py
+++ docs/conf.py
@@ -7,39 +7,39 @@ import os
 import sys
 
 # Add source directory to path for autodoc
-sys.path.insert(0, os.path.abspath('../src'))
+sys.path.insert(0, os.path.abspath("../src"))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'hsmkey'
-copyright = '2025, Kushal Das'
-author = 'Kushal Das'
-release = '0.1.0'
+project = "hsmkey"
+copyright = "2025, Kushal Das"
+author = "Kushal Das"
+release = "0.2.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon',
-    'sphinx_autodoc_typehints',
+    "sphinx.ext.autodoc",
+    "sphinx.ext.viewcode",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
 ]
 
-templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 # Napoleon settings for Google-style docstrings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = False
 
 # Autodoc settings
-autodoc_member_order = 'bysource'
-autodoc_typehints = 'description'
+autodoc_member_order = "bysource"
+autodoc_typehints = "description"
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
